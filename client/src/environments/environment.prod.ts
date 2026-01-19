701d5ba13341ebcb7132bd4089ef4d0ac7bc8237
Fabrizio Alonso Santi Guerrero <u202411774@upc.edu.pe>
2026-01-19 17:26:46 -0500

    hotfix: url api fixed

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://turistas-cp-production.up.railway.app/',
+  apiUrl: 'https://turistas-cp-production.up.railway.app',
   teamName: 'Team Turistas'
 };
